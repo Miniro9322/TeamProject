512bdd1594920a2babd343aaaec581f57c4a96ed
김광환 <louie1004@naver.com>
2026-07-09 14:44:55 +0900

    영웅 유닛 idle, attack 모션 구현

---
+++ Assets/Script/Hero/HeroAnimHash.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public static class HeroAnimHash
+{
+    public static int idle = Animator.StringToHash("Idle");
+    public static int attack = Animator.StringToHash("Attack");
+}
