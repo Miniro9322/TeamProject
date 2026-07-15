c42d455627124c9adb20aa6ca06d47c6d37e67f1
김지훈 <solev0025@gmail.com>
2026-07-15 18:01:53 +0900

    0715 정리
    -유닛 재배치 기능 추가(마우스 클릭)
    -일부 메서드명 변경

--- Assets/Script/Map/Test/UI/PanelData.cs
+++ Assets/Script/Map/Test/UI/PanelData.cs
@@ -8,4 +8,5 @@ public class PanelData
     public string[] Units = Array.Empty<string>();
     public bool ShowPath;
     public int UnitIndex = -1;
+    public bool Holding; // 유닛을 집은 이동 중 상태 → 이때 테스트 버튼 비활성화.
 }
