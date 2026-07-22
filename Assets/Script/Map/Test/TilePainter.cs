2b4f65608525fba4588d2d0e2700df797bdd6917
김지훈 <solev0025@gmail.com>
2026-07-22 18:52:58 +0900

    07-22 정리
    ## 맵 디자인 변경
     - 모듈 A = 숲 테마
     - 모듈 B = 광산 테마(현재  반영은 안됨)
     - 모듈 C = 화산지대 테마(현재 반영은 안됨)
     - 프리뷰용 씬 생성(MapThemePreview.unity)
     - 셰이더 관련 문제로 인한 TilePainter.cs 수정

--- Assets/Script/Map/Test/TilePainter.cs
+++ Assets/Script/Map/Test/TilePainter.cs
@@ -57,13 +57,10 @@ public class TilePainter : MonoBehaviour
 
     private void Restore(Tile tile)
     {
-        _mpb ??= new MaterialPropertyBlock();
         foreach (Renderer r in Renderers(tile))
         {
             if (r == null) continue;
-            r.GetPropertyBlock(_mpb);
-            _mpb.Clear();
-            r.SetPropertyBlock(_mpb);
+            r.SetPropertyBlock(null);
         }
     }
 
