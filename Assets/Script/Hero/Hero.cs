00848f13ae3f4cadcbc82a7bde656c116b1fcf75
김광환 <louie1004@naver.com>
2026-07-23 17:17:27 +0900

    영웅 업그레이드 데이터 추가

--- Assets/Script/Hero/Hero.cs
+++ Assets/Script/Hero/Hero.cs
@@ -7,8 +7,8 @@ using VContainer;
 
 public class Hero : MonoBehaviour, IDamageAble, IPlaceAble, IUnit
 {
-    [SerializeField] private AttackDataSO[] basePattern;
-    public AttackDataSO[] BasePattern => basePattern;
+    [SerializeField] private List<AttackDataSO> basePattern;
+    public List<AttackDataSO> BasePattern => basePattern;
 
     [SerializeField] private List<AttackSelectorSO> selectors = new();
     public List<AttackSelectorSO> Selectors => selectors;
@@ -350,5 +350,12 @@ public class Hero : MonoBehaviour, IDamageAble, IPlaceAble, IUnit
         origin = board.WorldToCell(transform.position);
         if (board.TryGetCell(origin, out Tile current))
             currentTile = current;
+    }
+
+    public void ExchangeAttackDatas(List<AttackDataSO> datas, List<AttackSelectorSO> selectors, List<AttackProcSO> procs)
+    {
+        this.basePattern = datas;
+        this.selectors = selectors;
+        this.procs = procs;
     }
 }
