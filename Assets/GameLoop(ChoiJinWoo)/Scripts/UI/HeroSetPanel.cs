01a42d2aecf5a96d965abb28edabf312f30f917d
김광환 <louie1004@naver.com>
2026-07-28 09:32:12 +0900

    히어로 패널에 힐러 추가

--- Assets/GameLoop(ChoiJinWoo)/Scripts/UI/HeroSetPanel.cs
+++ Assets/GameLoop(ChoiJinWoo)/Scripts/UI/HeroSetPanel.cs
@@ -63,7 +63,6 @@ public class HeroSetPanel : MonoBehaviour
             case "SpearMan": return HeroType.SpearMan;
             case "Mage": return HeroType.Mage;
             case "THS": return HeroType.THS;
-            case "Healer": return HeroType.Healer;
             default: return 0;
         }
     }
