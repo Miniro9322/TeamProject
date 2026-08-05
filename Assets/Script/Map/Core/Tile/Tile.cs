67effc97499d4ffe41256f4792470e0486e5c600
김지훈 <solev0025@gmail.com>
2026-08-05 13:16:09 +0900

    08-05 불 효과 타일 연결
    - tile.cs 에서 진입/이탈 신호
    - FireReceiver로 적 쪽 효과 수신

--- Assets/Script/Map/Core/Tile/Tile.cs
+++ Assets/Script/Map/Core/Tile/Tile.cs
@@ -99,7 +99,7 @@ public partial class Tile : MonoBehaviour
     {
         OccupantObject = go;
         State.Occupant = kind;
-        EnterFire(go);
+
     }
 
     //배치되어 있는 유닛을 해제 후 반환.
@@ -109,7 +109,7 @@ public partial class Tile : MonoBehaviour
 
         OccupantObject = null;
         State.Occupant = OccupantKind.None;
-        ExitFire(go);
+
 
         return go;
     }
@@ -139,28 +139,10 @@ public partial class Tile : MonoBehaviour
         {
             return;
         }
+
+        FireReceiver.ReceiveExit(this, enemy.transform);
     }
 
-    //불 칸일 때만 이 유닛을 불 줄 맨 뒤에 세운다.
-    private void EnterFire(GameObject unitObject)
-    {
-        if (!IsFire)
-        {
-            return;
-        }
 
-        BurnClock.EnterFire(this, unitObject);
-    }
-
-    //불 칸일 때만 이 유닛을 불 줄에서 뺀다.
-    private void ExitFire(GameObject unitObject)
-    {
-        if (!IsFire)
-        {
-            return;
-        }
-
-        BurnClock.ExitFire(this, unitObject);
-    }
 }
     
