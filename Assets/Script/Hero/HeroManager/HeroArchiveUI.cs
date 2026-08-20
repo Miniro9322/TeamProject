f3e882ad0ef3e792f31371735601aff4ada80dda
김광환 <louie1004@naver.com>
2026-08-20 11:50:40 +0900

    영웅 도감 추가

--- Assets/Script/Hero/HeroManager/HeroArchiveUI.cs
+++ Assets/Script/Hero/HeroManager/HeroArchiveUI.cs
@@ -1,4 +1,7 @@
+using Cysharp.Threading.Tasks;
+using System.Threading;
 using TMPro;
+using UnityEditor.Sprites;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -10,33 +13,107 @@ public class HeroArchiveUI : MonoBehaviour
 
     [SerializeField] private Image mainImage;
     [SerializeField] private TextMeshProUGUI nameText;
+    [SerializeField] private TextMeshProUGUI descText;
     [SerializeField] private Button exitButton;
-    private Animator bookAnimator;
+    [SerializeField] private Animator bookAnimator;
+    [SerializeField] private CanvasGroup leftPanel;
+    [SerializeField] private CanvasGroup rightPanel;
+    private float fadeDuration = 0.35f;
+    private CancellationTokenSource revealCts;
+    private bool isBookOpening = false;
 
-    private void OnEnable()
+    private void Awake()
     {
         BuildList();
     }
 
-    private void OnDisable()
-    {
-        ClearList();
-    }
+    //private void OnDisable()
+    //{
+    //    ClearList();
+    //}
     private void BuildList()
     {
-        ClearList();
+        //ClearList();
         foreach (HeroData data in registry.AllHeroDatas)
         {
             HeroArchiveItem item = Instantiate(itemPrefab, listContent);
             item.Setup(data, OnHeroArchiveClicked);
         }
+        HeroData heroData = registry.AllHeroDatas[0];
+        mainImage.sprite = heroData.Icon;
+        nameText.text = DataTableManager.StringTable.Get(heroData.HeroNameKey);
+        descText.text = DataTableManager.StringTable.Get(heroData.HeroDescriptionKey);
     }
 
     private void OnHeroArchiveClicked(HeroData picked)
     {
+        PlayOpenAndReveal();
         mainImage.sprite = picked.Icon;
         nameText.text = DataTableManager.StringTable.Get(picked.HeroNameKey);
+        descText.text = DataTableManager.StringTable.Get(picked.HeroDescriptionKey);
     }
+
+    private void PlayOpenAndReveal()
+    {
+        CancelReveal();
+        if (bookAnimator != null)
+        {
+            bookAnimator.ResetTrigger("Open");
+            bookAnimator.SetTrigger("Open");
+        }
+        SetRevealAlpha(0f);
+        isBookOpening = true;
+        revealCts = new CancellationTokenSource();
+        RevealAfterOpen(revealCts).Forget();
+    }
+
+    private void CancelReveal()
+    {
+        revealCts?.Cancel();
+        revealCts?.Dispose();
+        revealCts = null;
+        isBookOpening = false;
+    }
+
+    private void SetRevealAlpha(float a)
+    {
+        leftPanel.alpha = a;
+        rightPanel.alpha = a;
+    }
+
+    private async UniTask RevealAfterOpen(CancellationTokenSource own)
+    {
+        CancellationToken token = own.Token;
+        try
+        {
+            float t = 0f;
+            float revealDelay = 0.5f;
+
+            while (t < revealDelay)
+            {
+                t += Time.unscaledDeltaTime;
+                await UniTask.Yield(token);
+            }
+            isBookOpening = false;
+
+            t = 0f;
+            while (t < fadeDuration)
+            {
+                t += Time.unscaledDeltaTime;
+                SetRevealAlpha(Mathf.Clamp01(t / fadeDuration));
+                await UniTask.Yield(token);
+            }
+            SetRevealAlpha(1f);
+        }
+        finally
+        {
+            if (revealCts == own)
+            {
+                isBookOpening = false;
+            }
+        }
+    }
+
     private void ClearList()
     {
         for (int i = listContent.childCount - 1; i >= 0; i--)
