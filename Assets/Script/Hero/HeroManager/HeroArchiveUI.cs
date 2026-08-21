e755805a86efb5c51cfab28e07149a7cf852d5f0
김광환 <louie1004@naver.com>
2026-08-21 10:08:26 +0900

    메인씬 수정

--- Assets/Script/Hero/HeroManager/HeroArchiveUI.cs
+++ Assets/Script/Hero/HeroManager/HeroArchiveUI.cs
@@ -2,7 +2,6 @@ using Cysharp.Threading.Tasks;
 using System.Collections.Generic;
 using System.Threading;
 using TMPro;
-using UnityEditor.Sprites;
 using UnityEngine;
 using UnityEngine.UI;
 
--- Assets/Script/Hero/HeroManager/HeroInventory.cs
+++ Assets/Script/Hero/HeroManager/HeroInventory.cs
@@ -1,7 +1,5 @@
 using System.Collections.Generic;
-using UnityEditor.Rendering;
 using UnityEngine;
-using UnityEngine.EventSystems;
 using UnityEngine.Pool;
 
 public class HeroInventory : MonoBehaviour
