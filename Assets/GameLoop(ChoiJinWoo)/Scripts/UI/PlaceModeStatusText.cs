48aa8c8db736689423ee1e3aa8477a0d2d0e5854
최진우 <justly2000@naver.com>
2026-08-31 18:09:14 +0900

    재배치 및 회수 텍스트 stringtable 적용, 보스 연출 안나오는 버그 수정

--- Assets/GameLoop(ChoiJinWoo)/Scripts/UI/PlaceModeStatusText.cs
+++ Assets/GameLoop(ChoiJinWoo)/Scripts/UI/PlaceModeStatusText.cs
@@ -1,7 +1,7 @@
 using TMPro;
 using UnityEngine;
 
-// 재배치/제거 모드 상태를 텍스트로 보여준다. MapView의 상태 변경 이벤트로만 갱신되고 Update 폴링은 하지 않는다.
+// 재배치/제거 모드 상태를 텍스트로 보여준다. MapView의 상태 변경 이벤트와 언어 변경 이벤트로만 갱신되고 Update 폴링은 하지 않는다.
 public class PlaceModeStatusText : MonoBehaviour
 {
     [SerializeField] private MapView view;
@@ -10,23 +10,26 @@ public class PlaceModeStatusText : MonoBehaviour
     private void OnEnable()
     {
         view.OnStateChanged += Refresh;
+        LocalizeTextManager.OnLanguageChanged += Refresh;
         Refresh();
     }
 
     private void OnDisable()
     {
         view.OnStateChanged -= Refresh;
+        LocalizeTextManager.OnLanguageChanged -= Refresh;
     }
 
     private void Refresh()
     {
+        var stringTable = DataTableManager.StringTable;
         if (view.IsRemoving)
         {
-            text.text = "회수할 영웅을 선택 해주세요.\nesc키로 취소";
+            text.text = stringTable.Get("Ui_PlaceModeRemove");
         }
         else if (view.IsReplacing)
         {
-            text.text = view.IsHolding ? "배치할 칸을 선택해주세요\nesc키로 취소." : "재배치할 영웅을 선택 해주세요.\nesc키로 취소";
+            text.text = stringTable.Get(view.IsHolding ? "Ui_PlaceModeReplaceHold" : "Ui_PlaceModeReplace");
         }
         else
         {
