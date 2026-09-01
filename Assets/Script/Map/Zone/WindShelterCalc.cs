b1f2dcfa91e697d3943e48beaf481a3a183162fd
김지훈 <solev0025@gmail.com>
2026-09-01 11:01:19 +0900

    09-01 사막 지대 이펙트 수정
    - 바람 속도 느리게 조정
    - 고지쪽 이펙트 반영
    - 누락된 코어 표면 프리팹 첨부

--- Assets/Script/Map/Zone/WindShelterCalc.cs
+++ Assets/Script/Map/Zone/WindShelterCalc.cs
@@ -19,7 +19,8 @@ public class WindShelterCalc
         WindShelterData data = new();
         CollectTiles(cells, data);
         CollectHighs();
-        ResolveGrounds(data);
+        ResolveShelters(groundTiles, data);
+        ResolveShelters(highTiles, data);
         return data;
     }
 
@@ -87,14 +88,14 @@ public class WindShelterCalc
         highCells.Add(cell);
     }
 
-    // 모든 평지 타일에 대해 바람 막힘 여부를 계산해서 채운다.
-    private void ResolveGrounds(WindShelterData data)
+    // 타일 목록마다 이웃 고지를 확인해 바람 막힘 정보를 계산해서 채운다.
+    private void ResolveShelters(List<Tile> tiles, WindShelterData data)
     {
-        for (int groundIndex = 0; groundIndex < groundTiles.Count; groundIndex++)
+        for (int index = 0; index < tiles.Count; index++)
         {
-            Tile groundTile = groundTiles[groundIndex];
-            WindShelter shelter = ResolveShelter(groundTile.Coord);
-            data.KeepShelter(groundTile, shelter);
+            Tile tile = tiles[index];
+            WindShelter shelter = ResolveShelter(tile.Coord);
+            data.KeepShelter(tile, shelter);
         }
     }
 
